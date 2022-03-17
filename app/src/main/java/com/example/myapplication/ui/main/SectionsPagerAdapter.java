956511a36d4cb6d94043b0de9664cd74b415ddba
root12332 <2823051516@qq.com>
2022-03-17 11:59:41 +0800

    第一次提交

---
+++ app/src/main/java/com/example/myapplication/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,64 @@
+package com.example.myapplication.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.myapplication.GerenFragment;
+import com.example.myapplication.NewsFragment;
+import com.example.myapplication.R;
+import com.example.myapplication.ShouyeFragment;
+import com.example.myapplication.TecFragment;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3, R.string.tab_text_4 };
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        switch (position){
+            case 0:
+                return new ShouyeFragment();
+
+            case 1:
+                return new GerenFragment();
+            case 2:
+                return new NewsFragment();
+            case 3:
+                return new TecFragment();
+        }
+
+
+
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 4;
+    }
+}
